34019fd2f41b8d5dde40416a03bd897faf8f1201
KUTlime <kutlime@gmail.com>
2020-06-07 11:13:08 +0200

    v1.0.1: Version build update About page update with version and build number.

--- HubSender/HubSender/ViewModels/AboutViewModel.cs
+++ HubSender/HubSender/ViewModels/AboutViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System.Windows.Input;
+﻿using System.Reflection;
+using System.Windows.Input;
 
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 
 namespace HubSender.ViewModels
 {
@@ -11,8 +13,13 @@ namespace HubSender.ViewModels
 		{
 			Title = "About";
 			OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/KUTlime/Azure-Notification-Hub"));
+
 		}
 
 		public ICommand OpenWebCommand { get; }
+
+		public string VersionNumber { get; } = VersionTracking.CurrentVersion;
+
+		public string BuildNumber { get; } = VersionTracking.CurrentBuild;
 	}
 }
